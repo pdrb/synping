1bfcae13356c8f8aad8b37b34ed0963920c53540
Pedro Buteri Gonring <pedro@bigode.net>
2017-02-03 21:35:49 -0200

    Remove MANIFEST

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='synping',
-    packages = ['synping'],
     version='0.2',
     description='Ping hosts using tcp syn packets',
+    long_description=long_description,
     author='Pedro Buteri Gonring',
     author_email='pedro@bigode.net',
     url='https://github.com/pdrb/synping',
-    download_url = 'https://github.com/pdrb/synping/tarball/0.1',
-    keywords = ['ping', 'syn', 'tcp', 'packets'],
+    license='MIT',
     classifiers = [],
+    keywords = 'ping syn tcp packets',    
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['argparse'],
+    entry_points={
+        'console_scripts': ['synping=synping:main'],
+    },
 )
